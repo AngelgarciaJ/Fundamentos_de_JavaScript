7acef6c6e6b8dac150b3a3a18d98e58494e67ccc
Angel García <93614402+AngelgarciaJ@users.noreply.github.com>
2022-07-14 14:13:16 -0500

    Update and rename 1_proyecto_crehana to 1_proyecto_crehana.js

--- 1_proyecto_crehana.js
+++ 1_proyecto_crehana.js
@@ -1,7 +1,10 @@
+// este es el avance dos; El código calcula el IVA (18% -> 0.18) y precio final de un producto.
 
+// declaracion de variables 
 const iva = 0.18;
 let Total;
 
+// funcion que hace el calculo
 function calcularIva(precio){
     let ivaProducto = precio * iva;
     let Total = precio - ivaProducto;
